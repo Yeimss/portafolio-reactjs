c6d6ace16609dc9290c95b5de377b7c8afa4f27b
Yeimss <jamesherrera364@gmail.com>
2025-08-29 21:36:42 -0500

    Basic design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,20 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="bg-sky-400 grid grid-cols-3 py-4 border-top border-t-4 border-indigo-500">
+        <footer className="bg-indigo-300 grid grid-cols-3 py-4 border-top border-t-3 border-indigo-500 w-1/1">
             <div className="flex justify-center">
                 <a target="_blank" href="https://github.com/Yeimss">
-                    <FaGithub size={24}/> 
+                    <FaGithub size={30} color="rgba(255,255,255)"/> 
                 </a>
             </div>
             <div className="flex justify-center">
                 <a target="_blank" href="https://www.linkedin.com/in/james-herreraa/">
-                    <FaLinkedin size={24}/>
+                    <FaLinkedin size={30} color="rgba(255,255,255)"/>
                 </a>
             </div>
             <div className="flex justify-center">
                 <a target="_blank" href="https://www.instagram.com/james.h_no/">
-                    <FaInstagram size={24}/>
+                    <FaInstagram size={30} color="rgba(255,255,255)"/>
                 </a> 
             </div>
         </footer>
---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import BoardCard from "./utils/Card/BoardCard";
+const Main = () => {
+    return (
+        <main>
+            <BoardCard/>
+        </main>
+    )
+}
+
+export default Main;
